3562e7d4216a75eb2723c157a7f553478d62e6df
enseiha <jingbo15722@gmail.com>
2023-08-29 12:53:06 +0900

    jwt persist

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const BASE_URL= 'http://136.187.80.43:18080'
-export const IMG_BASE_URL= 'http://136.187.116.134:18080/web/images/'
+const BASE_URL= 'http://136.187.82.145:18080'
+export const IMG_BASE_URL= 'http://136.187.116.134:18080/web/'
 export default axios.create({
     baseURL: BASE_URL
 });
